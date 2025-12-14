abc4d558f7fd91fac4993d604664508c28586d35
SelenEris <lea.banquart@gmail.com>
2025-12-14 01:39:03 +0100

    Jour 6

--- AdventOfCode/Riddle.cs
+++ AdventOfCode/Riddle.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Collections.Immutable;
 using System.Collections;
+using System.Net.Mail;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AdventOfCode
 {
@@ -32,7 +34,7 @@ namespace AdventOfCode
                 case 0:
                     break;
                 case 1:
-                    if(Number == 1)CodeSafe1();
+                    if (Number == 1) CodeSafe1();
                     else CodeSafe2();
                     break;
                 case 2:
@@ -50,6 +52,10 @@ namespace AdventOfCode
                 case 5:
                     if (Number == 1) FreshIngredient1();
                     else FreshIngredient2();
+                    break;
+                case 6:
+                    if (Number == 1) MathProblem1();
+                    else MathProblem2();
                     break;
 
             }
@@ -108,7 +114,7 @@ namespace AdventOfCode
                         }
                         else
                         {
-                            if(numberTick == countSafe) countSafe = 0;
+                            if (numberTick == countSafe) countSafe = 0;
                             else countSafe = 100 - (numberTick - countSafe);
                         }
                     }
@@ -186,10 +192,10 @@ namespace AdventOfCode
                         else
                         {
                             // If the number of tick makes the count safe lesser than 0, then it automatically pass by 0
-                            if (countSafe !=0) count0++;
+                            if (countSafe != 0) count0++;
                             if (numberTick == countSafe) countSafe = 0;
-                            else countSafe = 100 - (numberTick - countSafe); 
-                            
+                            else countSafe = 100 - (numberTick - countSafe);
+
                         }
                     }
                     line = sr.ReadLine();
@@ -225,10 +231,10 @@ namespace AdventOfCode
                 sr.Close();
 
                 //Split the input into different ranges of IDs
-                string [] idRanges = line.Split(',') ?? [];
+                string[] idRanges = line.Split(',') ?? [];
 
                 //Loop on the ranges
-                foreach(string idRange in idRanges)
+                foreach (string idRange in idRanges)
                 {
                     //Preperation of variables used for comparaison
                     string idStart = idRange.Split('-')[0];
@@ -241,10 +247,10 @@ namespace AdventOfCode
                     {
                         //Comparaison between the two halfs of each ID
                         string idIndexString = idIndexInteger.ToString();
-                        string firstPart = idIndexString.Substring(0,idIndexString.Length / 2);
+                        string firstPart = idIndexString.Substring(0, idIndexString.Length / 2);
                         string lastPart = idIndexString.Substring(idIndexString.Length / 2);
                         //If two parts are equal, the ID is fake
-                        if(firstPart == lastPart) countInvalidIDs+=idIndexInteger;
+                        if (firstPart == lastPart) countInvalidIDs += idIndexInteger;
                         idIndexInteger++;
                     }
                 }
@@ -347,7 +353,7 @@ namespace AdventOfCode
                     }
 
                     //To find the second digit, the search is done from the position following the first digit found, to the end
-                    for (int j = indexFirstHighNumber+1; j < listIndividualJoltage.Count; j++)
+                    for (int j = indexFirstHighNumber + 1; j < listIndividualJoltage.Count; j++)
                     {
                         if (listIndividualJoltage[j] > secondHighNumber)
                         {
@@ -458,13 +464,13 @@ namespace AdventOfCode
                 line = sr.ReadLine() ?? string.Empty;
 
                 //Loop on the lines in the file to make a map : in each line, there is a list of rolls
-                for (int i=0;  line != null; i++)
+                for (int i = 0; line != null; i++)
                 {
                     List<char> listRolls = line.ToList();
                     lines.Add(listRolls);
                     line = sr.ReadLine();
                 }
-                    
+
                 sr.Close();
 
                 //Now that the map is done, the search begins
@@ -472,12 +478,12 @@ namespace AdventOfCode
                 int totalRolls = 0;
 
                 //First loop on the lines
-                for (int i=0; i < lines.Count ; i++)
+                for (int i = 0; i < lines.Count; i++)
                 {
                     List<char> listRolls = lines[i];
 
                     //Second loop on the rolls
-                    for (int j=0; j < listRolls.Count; j++)
+                    for (int j = 0; j < listRolls.Count; j++)
                     {
                         char roll = listRolls[j];
 
@@ -487,16 +493,16 @@ namespace AdventOfCode
                         if (roll == '@')
                         {
                             //It's a square where the limits are 'i-1','i+1' and 'j-1', 'j+1', but making shure we don't go over the map
-                           
+
                             for (int k = (i != 0) ? i - 1 : i; k < lines.Count && k < i + 2; k++)
                             {
-                                for (int l = (j != 0) ? j - 1 : j; l < listRolls.Count && l < j + 2 ; l++)
+                                for (int l = (j != 0) ? j - 1 : j; l < listRolls.Count && l < j + 2; l++)
                                 {
                                     if (lines[k][l] == '@' && (k != i || l != j)) countCloseRolls++;
                                 }
                             }
 
-                            if(countCloseRolls < 4) totalRolls++;
+                            if (countCloseRolls < 4) totalRolls++;
                         }
                     }
                 }
@@ -547,7 +553,7 @@ namespace AdventOfCode
         }
         #endregion
 
-        #region Day5
+        #region Day 5
         /// <summary>
         /// Calculation the fresh ingredients available
         /// </summary>
@@ -585,7 +591,7 @@ namespace AdventOfCode
                     long id = long.Parse(line);
 
                     //loop to search for the right range, if it exists
-                    foreach(Tuple<long, long> range in listRanges)
+                    foreach (Tuple<long, long> range in listRanges)
                     {
                         if (id >= range.Item1 && id <= range.Item2)
                         {
@@ -619,7 +625,7 @@ namespace AdventOfCode
                 //Declaration of variables
                 string line;
                 long countIdsOK = 0;
-                List<List <long>> listRanges = new List<List<long>>();
+                List<List<long>> listRanges = new List<List<long>>();
 
                 StreamReader sr = new StreamReader(FilePath);
 
@@ -628,13 +634,13 @@ namespace AdventOfCode
                 //Loop on the firsts lines in the file to keep the ranges
                 while (!string.IsNullOrEmpty(line))
                 {
-                    countLine ++;
+                    countLine++;
                     //Preperation of variables used for comparaison
                     long idStart = long.Parse(line.Split('-')[0]);
                     long idEnd = long.Parse(line.Split('-')[1]);
                     bool hasRangeBeenDiscarded = false;
                     //loop to verify the overlapping ranges
-                    for(int i = 0; i < listRanges.Count; i++)
+                    for (int i = 0; i < listRanges.Count; i++)
                     {
                         //If the first part of the current range is in one of the other ranges
                         if (idStart >= listRanges[i][0] && idStart <= listRanges[i][1])
@@ -661,7 +667,7 @@ namespace AdventOfCode
 
                         }
                         //If the current range entirely overlap an other range
-                        else if(idStart < listRanges[i][0] && idEnd > listRanges[i][1])
+                        else if (idStart < listRanges[i][0] && idEnd > listRanges[i][1])
                         {
                             //the other range is discarded
                             listRanges.RemoveAt(i);
@@ -670,19 +676,216 @@ namespace AdventOfCode
                     if (!hasRangeBeenDiscarded && idStart <= idEnd)
                     {
                         listRanges.Add(new List<long> { idStart, idEnd });
-                        listRanges.Sort((x,y) => x[0].CompareTo(y[0]));
+                        listRanges.Sort((x, y) => x[0].CompareTo(y[0]));
                     }
 
                     line = sr.ReadLine();
                 }
 
-                for (int i = 0; i<listRanges.Count; i++)
+                for (int i = 0; i < listRanges.Count; i++)
                 {
 
                     countIdsOK += (listRanges[i][1] - listRanges[i][0]) + 1;
                 }
                 sr.Close();
                 Solution = countIdsOK;
+            }
+            else
+            {
+                Console.WriteLine("The path to the file is not correct.");
+            }
+        }
+        #endregion
+
+        #region Day6
+        /// <summary>
+        /// Calculation the fresh ingredients available
+        /// </summary>
+        public void MathProblem1()
+        {
+            if (!string.IsNullOrEmpty(FilePath) && File.Exists(FilePath))
+            {
+                //Declaration of variables
+                string line;
+                long sumCalculus = 0;
+                List<List<string>> listAll = new List<List<string>>();
+
+                StreamReader sr = new StreamReader(FilePath);
+
+                line = sr.ReadLine();
+
+                //Loop to get all the file in the list
+                while (!string.IsNullOrEmpty(line))
+                {
+                    //Preperation of variables used for comparaison
+                    listAll.Add(line.Split(' ').ToList());
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+
+                //Now, time to determine the operand
+                List<string> listOperand = new List<string>();
+                listOperand = listAll.Last();
+                listAll.Remove(listOperand);
+
+                //Cleaning the empty parts
+                listAll.ForEach(x => x.RemoveAll(y => y == string.Empty));
+                listOperand.RemoveAll(x => x == string.Empty);
+
+                //Loop on all the operands to create the calculus
+                for (int i = 0; i < listOperand.Count; i++)
+                {
+                    long resultCalculus = 0;
+                    if (listOperand[i] == "+")
+                    {
+                        for (int j = 0; j < listAll.Count; j++)
+                            resultCalculus += long.Parse(listAll[j][i]);
+                    }
+                    if (listOperand[i] == "*")
+                    {
+                        resultCalculus = 1;
+                        for (int j = 0; j < listAll.Count; j++)
+                            resultCalculus *= long.Parse(listAll[j][i]);
+                    }
+                    sumCalculus += resultCalculus;
+                }
+                Solution = sumCalculus;
+            }
+            else
+            {
+                Console.WriteLine("The path to the file is not correct.");
+            }
+        }
+
+        /// <summary>
+        /// Calculation the fresh ingredients available
+        /// </summary>
+        public void MathProblem2()
+        {
+            if (!string.IsNullOrEmpty(FilePath) && File.Exists(FilePath))
+            {
+                //Declaration of variables
+                string line;
+                long sumCalculus = 0;
+                List<List<string>> listAll = new List<List<string>>();
+
+                List<string> listTest = new List<string>();
+                StreamReader sr = new StreamReader(FilePath);
+
+                line = sr.ReadLine();
+
+                //Loop to get all the file in the list
+                while (!string.IsNullOrEmpty(line))
+                {
+                    //Preperation of variables used for comparaison
+                    listTest.Add(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+
+                //Now, time to determine the operand
+                List<string> listOperand = new List<string>();
+                string listOperandString = listTest.Last();
+                listTest.RemoveAt(listTest.Count - 1);
+
+                //keeping empty part in number
+                int lastOperand = 0;
+                for (int i = 0; i < listOperandString.Length; i++)
+                {
+                    if (listOperandString[i] != ' ')
+                    {
+                        if (i != 0)
+                        {
+                            List<string> listCalculus = new List<string>();
+                            for (int j = 0; j < listTest.Count; j++)
+                            {
+                                string nombreAvecEspace = listTest[j].Substring(lastOperand, (i - lastOperand) - 1);
+                                listCalculus.Add(nombreAvecEspace);
+                            }
+                            listAll.Add(listCalculus);
+                        }
+                        lastOperand = i;
+
+                    }
+                    else if (i == listOperandString.Length - 1)
+                    {
+                        List<string> listCalculus = new List<string>();
+                        for (int j = 0; j < listTest.Count; j++)
+                        {
+                            string nombreAvecEspace = listTest[j].Substring(lastOperand, (listOperandString.Length - lastOperand));
+                            listCalculus.Add(nombreAvecEspace);
+                        }
+                        listAll.Add(listCalculus);
+                        lastOperand = i;
+                    }
+                }
+                listOperand = listOperandString.Split(' ').ToList();
+                listOperand.RemoveAll(x => x == string.Empty);
+
+                //Creating the empty new list with the number in right order 
+                List<List<string>> listRightOrder = new List<List<string>>();
+
+                foreach (List<string> listNumber in listAll)
+                {
+                    List<string> listTemp = new List<string>();
+                    foreach (string number in listNumber)
+                    {
+                        listTemp.Add(string.Empty);
+                    }
+                    listRightOrder.Add(listTemp);
+                }
+
+               //Filling the list with the correct numbers
+                for (int i = listAll.Count - 1; i >= 0; i--)
+                {
+                    List<string> listNumber = listAll[i];
+                    int nbrEmpty = 0;
+                    //The 'h' loop is used to keep track
+                    for(int h = 0; h < listNumber.Count && nbrEmpty < listNumber.Count; h++)
+                    {
+                        for (int j = 0; j < listNumber.Count; j++)
+                        {
+                            string number = listAll[i][j];
+                            if (listNumber[j].Last() != ' ')
+                            {
+                                listRightOrder[i][h] += number.Last();
+                            }
+                            listAll[i][j] = listAll[i][j].Substring(0, number.Length - 1);
+
+                            if (string.IsNullOrEmpty(listAll[i][j])) nbrEmpty++;
+
+                        }
+                    }
+                }
+
+                //Loop on all the operands to create the calculus
+                for (int i = 0; i < listOperand.Count; i++)
+                {
+                    long resultCalculus = 0;
+                    if (listOperand[i] == "+")
+                    {
+                        for (int j = 0; j < listRightOrder[i].Count; j++)
+                        {
+                            long nombre = 0;
+                            if (listRightOrder[i][j] != string.Empty) nombre = long.Parse(listRightOrder[i][j]);
+                            resultCalculus += nombre;
+                        }
+                    }
+                    if (listOperand[i] == "*")
+                    {
+                        resultCalculus = 1;
+                        for (int j = 0; j < listRightOrder[i].Count; j++)
+                        {
+                            long nombre = 1;
+                            if (listRightOrder[i][j] != string.Empty) nombre = long.Parse(listRightOrder[i][j]);
+                            resultCalculus *= nombre;
+                        }
+                    }
+                    sumCalculus += resultCalculus;
+                }
+                Solution = sumCalculus;
             }
             else
             {
